7149b0b43f9926b0f84dd525e3f65c472bca9880
LoiNguyennn <nguyenphucloi231@gmail.com>
2025-03-19 08:36:16 +0700

    #12-LoiNguyennn-File format tools

---
+++ ITTools.Core/FileFormat/XmlFormatter.cs
@@ -0,0 +1,29 @@
+﻿using ITTools.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FileFormat
+{
+    public class XmlFormatter : ITool
+    {
+        public string Name => "XML Formatter";
+        public string Description => "Formats an XML string with proper structure";
+        public string Category => "File Format Tools";
+        public string Execute(string input)
+        {
+            try
+            {
+                var doc = XDocument.Parse(input);
+                return doc.ToString();
+            }
+            catch
+            {
+                return "Invalid XML";
+            }
+        }
+    }
+}
