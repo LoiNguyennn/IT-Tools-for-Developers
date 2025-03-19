0477436e4b160d907910cfecc518c6dddce792e9
LoiNguyennn <nguyenphucloi231@gmail.com>
2025-03-19 08:55:34 +0700

    #10-LoiNguyennn-Number tools

---
+++ ITTools.Core/NumberPlugins/FibonacciGenerator.cs
@@ -0,0 +1,35 @@
+﻿using ITTools.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPlugins
+{
+    public class FibonacciGenerator : ITool
+    {
+        public string Name => "Fibonacci Generator";
+        public string Description => "Generates a Fibonacci sequence up to n";
+        public string Category => "Number Tools";
+        public string Execute(string input)
+        {
+            if (!int.TryParse(input, out int n) || n <= 0)
+            {
+                return "Invalid input (must be a positive integer)";
+            }
+
+            if (n > 46) // Limit to avoid overflow with long
+            {
+                return "Input too large (max 46)";
+            }
+
+            var fib = new List<long> { 0, 1 };
+            for (int i = 2; i < n; i++)
+            {
+                fib.Add(fib[i - 1] + fib[i - 2]);
+            }
+            return string.Join(", ", fib);
+        }
+    }
+}
---
+++ ITTools.Core/NumberPlugins/PrimeNumberChecker.cs
@@ -0,0 +1,27 @@
+﻿using ITTools.Core.Models;
+
+namespace NumberPlugins
+{
+    public class PrimeNumberChecker : ITool
+    {
+        public string Name => "Prime Number Checker";
+        public string Description => "Checks if a number is prime";
+        public string Category => "Number Tools";
+        public string Execute(string input)
+        {
+            if (!int.TryParse(input, out int num) || num < 2)
+            {
+                return "Not Prime (invalid input or less than 2)";
+            }
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return "Not Prime";
+                }
+            }
+            return "Prime";
+        }
+    }
+}
