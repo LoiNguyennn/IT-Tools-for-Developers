87e24dabbac89db9d6d9716ce772f7eb188dadab
LoiNguyennn <nguyenphucloi231@gmail.com>
2025-03-19 08:47:22 +0700

    #11-LoiNguyennn-Text analysis

---
+++ ITTools.Core/TextAnalysis/SentenceCounter.cs
@@ -0,0 +1,26 @@
+﻿using ITTools.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysis
+{
+    public class SentenceCounter : ITool
+    {
+        public string Name => "Sentence Counter";
+        public string Description => "Counts sentences in the input";
+        public string Category => "Text Analysis Tools";
+        public string Execute(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "0";
+            }
+            // Split by sentence-ending punctuation and remove empty entries
+            var sentences = input.Split(new[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            return sentences.Length.ToString();
+        }
+    }
+}
---
+++ ITTools.Core/TextAnalysis/VowelCounter.cs
@@ -0,0 +1,25 @@
+﻿using ITTools.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysis
+{
+    public class VowelCounter : ITool
+    {
+        public string Name => "Vowel Counter";
+        public string Description => "Counts vowels in the input text";
+        public string Category => "Text Analysis Tools";
+        public string Execute(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "0";
+            }
+            int count = input.Count(c => "aeiouAEIOU".Contains(c));
+            return count.ToString();
+        }
+    }
+}
